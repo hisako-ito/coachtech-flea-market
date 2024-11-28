b54beedba2495c673b7222f72abd5918e57b2844
hisako_ito <hisahisaito@gmail.com>
2024-11-29 02:13:48 +0900

    マイグレーションファイル追加

---
+++ src/app/Models/Item.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function condition()
+    {
+        return $this->belongsTo(Condition::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class,'category_item','category_id','item_id',);
+    }
+}
